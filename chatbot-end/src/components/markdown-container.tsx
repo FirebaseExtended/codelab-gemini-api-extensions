460771a5e3002706532d9d1f67e730d5dab56c45
Alex Astrum <alexastrum@gmail.com>
2024-04-16 17:04:16 -0400

    add details on markdown parser risks when used with llms

--- chatbot-end/src/components/markdown-container.tsx
+++ chatbot-end/src/components/markdown-container.tsx
@@ -7,15 +7,27 @@ export interface MarkdownContainerProps {
 }
 
 /**
- * Markdown container sanitizes and renders markdown text.
+ * Markdown container renders markdown text.
  *
  * @param props.markdown
  */
 const MarkdownContainer: React.FC<MarkdownContainerProps> = ({ markdown }) => {
   const htmlFromMarkdown = marked.parse(markdown) as string;
+  // Remove malformed HMTL
   const cleanHtml = DOMPurify.sanitize(htmlFromMarkdown);
 
   return (
+    /*
+    Note: Do NOT use this code in production if your LLM prompt might contain input data
+    from more than one user at any time.
+
+    Even though DOMPurify removes some XSS attack vectors, 
+    fixing [data exfiltration risks](https://embracethered.com/blog/)
+    is outside the scope of this demo.
+
+    If not sure, only display LLM outputs as text; get your code audited by an expert;
+    and never output unfiltered LLM output as runnable code, HTML, markdown, or URL attributes.
+    */
     <div
       dangerouslySetInnerHTML={{
         __html: cleanHtml,
