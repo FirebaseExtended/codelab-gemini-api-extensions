d4017578e70924e0c26ac6065cf56268a2c925bf
Alex Astrum <alexastrum@gmail.com>
2024-05-03 10:10:32 -0400

    fix node version and gallery code

--- chatbot-end/src/app/gallery/page.tsx
+++ chatbot-end/src/app/gallery/page.tsx
@@ -33,6 +33,7 @@ interface FirestoreImageData {
   id?: string;
   uid?: string;
   image?: string;
+  output?: string;
   createTime?: Timestamp;
   published?: boolean;
   flagUid?: string;
@@ -46,7 +47,7 @@ export default function Gallery() {
     () =>
       collection(
         getFirestore(),
-        `images`
+        `gallery`
       ) as CollectionReference<FirestoreImageData>,
     []
   );
@@ -150,7 +151,7 @@ export default function Gallery() {
           className="w-full flex justify-start items-center gap-4 my-4 px-4 group"
         >
           {image.image && <img src={image.image} alt="Image" width={200} />}
-          <div className="flex-1">TODO Description</div>
+          <div className="flex-1">{image.output}</div>
           {image.id && image.uid === uid && (
             <button
               type="button"
@@ -178,75 +179,5 @@ export default function Gallery() {
 
       <FileInputContainer onFileUpload={uploadImage} progress={progress} />
     </div>
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    //   <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-    //     <div className="flex flex-col gap-4">
-    //       <div className="flex gap-4">
-    //         <Image
-    //           src="/image-1.jpg"
-    //           alt="Image 1"
-    //           width={200}
-    //           height={200}
-    //         />
-    //         <Image
-    //           src="/image-2.jpg"
-    //           alt="Image 2"
-    //           width={200}
-    //           height={200}
-    //         />
-    //       </div>
-    //       <div className="flex gap-4">
-    //         <Image
-    //           src="/image-3.jpg"
-    //           alt="Image 3"
-    //           width={200}
-    //           height={200}
-    //         />
-    //         <Image
-    //           src="/image-4.jpg"
-    //           alt="Image 4"
-    //           width={200}
-    //           height={200}
-    //         />
-    //       </div>
-    //     </div>
-    //     <div className="flex flex-col gap-4">
-    //       <div className="flex gap-4">
-    //         <Image
-    //           src="/image-5.jpg"
-    //           alt="Image 5"
-    //           width={200}
-    //           height={200}
-    //         />
-    //         <Image
-    //           src="/image-6.jpg"
-    //           alt="Image 6"
-    //           width={200}
-    //           height={200}
-    //         />
-    //       </div>
-    //       <div className="flex gap-4">
-    //         <Image
-    //           src="/image-7.jpg"
-    //           alt="Image 7"
-    //           width={200}
-    //           height={200}
-    //         />
-    //         <Image
-    //           src="/image-8.jpg"
-    //           alt="Image 8"
-    //           width={200}
-    //           height={200}
-    //         />
-    //       </div>
-    //     </div>
-    //   </div>
-
-    //   <div className="flex justify-center mt-8">
-    //     <button className="rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-    //       <span className="text-lg font-semibold">Upload</span>
-    //     </button>
-    //   </div>
-    // </main>
   );
 }
